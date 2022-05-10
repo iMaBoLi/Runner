91520a081779ab105530cdec0d5d23640ee770d3
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-10 17:08:57 +0430

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,11 @@
 from . import bot
+from bot.functions.utils import load_plugins
 
-bot.run()
+async def setup():
+    LOGS.info("• Starting Setup Plugins . . .")
+    load_plugins("plugins")
+    LOGS.info("• Setup Plugins Completed!")
+    bot.me = await bot.get_me()
+    LOGS.info("• Alien UserBot Has Been Start Now!")
+
+bot.loop.run_until_complete(setup())
