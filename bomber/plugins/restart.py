983a616a9fcdc4bcfb27b725a476da556304556b
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 20:23:58 +0430

    Delete restart.py

--- bomber/plugins/restart.py
+++
@@ -1,8 +0,0 @@
-from bomber import bot
-from telethon import events
-import os
-
-@Cmd(pattern="(?i)^\/restart$", admin_only=True)
-async def restart(event):
-    await event.reply("**• Bomber Restarted!**")
-    os.system("bash start")
