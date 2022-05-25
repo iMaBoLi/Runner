1c89d60c96ada42183127ee6891bfaf6ffdd62d0
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 19:31:28 +0430

    Update __main__.py

--- bomber/__main__.py
+++ bomber/__main__.py
@@ -2,11 +2,11 @@ from . import bot
 from bomber.functions.utils import load_plugins
 
 async def setup():
-    LOGS.info("• Starting Setup Plugins . . .")
+    print("• Starting Setup Plugins . . .")
     load_plugins()
-    LOGS.info("• Setup Plugins Completed!")
+    print("• Setup Plugins Completed!")
     bot.me = await bot.get_me()
-    LOGS.info("• Bomber Has Been Start Now!")
+    print("• Bomber Has Been Start Now!")
 
 bot.loop.run_until_complete(setup())
 bot.run_until_disconnected()
