6585400bf60692ef1be1c5d5c815b9a7c05c71ea
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 21:12:46 +0430

    Update __main__.py

--- bomber/__main__.py
+++ bomber/__main__.py
@@ -1,4 +1,4 @@
-from . import bot
+from . import bot, ADMIN_ID
 from bomber.functions.utils import load_plugins
 
 async def setup():
@@ -6,6 +6,7 @@ async def setup():
     load_plugins()
     print("• Setup Plugins Completed!")
     bot.me = await bot.get_me()
+    bot.admin = await bot.get_entity(ADMIN_ID)
     print("• Bomber Has Been Start Now!")
 
 bot.loop.run_until_complete(setup())
