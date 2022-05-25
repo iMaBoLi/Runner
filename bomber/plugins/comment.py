da4f1e92180f6b3ae108350df8bacdafa74c1486
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-26 02:37:20 +0430

    Update comment.py

--- bomber/plugins/comment.py
+++ bomber/plugins/comment.py
@@ -1,31 +1,22 @@
 import os
-os.system("pip install instaloader")
 os.system("pip install instagrapi")
 os.system("pip install pillow")
+from bomber import bot
+from bomber.events import Cmd
 import instaloader
 import instagrapi
 import re
 import random
 
-insta = instaloader.Instaloader()
-insta.login("mx_aboli", "abol83@#")       
-profile = instaloader.Profile.from_username(insta.context, "sheykh_apex")
-list = []
-fols = profile.get_followers()
-for x in fols:
-    list.append(str(x))
-
 insta = instagrapi.Client()
-insta.load_settings("session.json")
-media_id = insta.media_id(insta.media_pk_from_url("https://www.instagram.com/tv/CdxoYMhl_to/?igshid=YmMyMTA2M2Y="))
-
-from bomber import bot
-from bomber.events import Cmd
 
 @Cmd(pattern="/add")
 async def add(event):
+    insta.load_settings("bomber/session.json")
+    media_id = insta.media_id(insta.media_pk_from_url("https://www.instagram.com/tv/CdxoYMhl_to/?igshid=YmMyMTA2M2Y="))
     edit = await event.edit("**• Starting . . .**")
     count = 0
+    list = open("bomber/users.txt", "r").readlines()
     for i in range(1000):
         rand = random.choice(list)
         username = re.search("<Profile (.*) \((.*)\)>", rand)
