3cff918429a9f04bc03ac97248d65266de803d2c
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 20:01:19 +0430

    Update info.py

--- bomber/plugins/info.py
+++ bomber/plugins/info.py
@@ -5,14 +5,14 @@ from bomber.database import DB
 @bot.on(events.NewMessage(pattern="(?i)\/myinfo"))
 async def start(event):
     info = await bot.get_entity(event.sender_id)
-    datainfo = DB.get_key("BOT_USERS").[info.id]
+    datainfo = DB.get_key("BOT_USERS")
     await event.reply(f"""
 **• Your Information:**
 
 **• ID:** ( `{info.id}` )
-**• Coins:** ( `{datainfo["coins"]}` )
-**• Invites:** ( `{datainfo["invites"]}` )
-**• Rank:** ( `{datainfo["rank"]}` )
+**• Coins:** ( `{datainfo.[info.id]["coins"]}` )
+**• Invites:** ( `{datainfo.[info.id]["invites"]}` )
+**• Rank:** ( `{datainfo.[info.id]["rank"]}` )
 
 **🆔 @MxBomber_Bot**
 """)
