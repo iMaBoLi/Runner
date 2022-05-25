fb88283e6ad1d787fc2989eef7705cfa6c7878d1
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 20:20:43 +0430

    Update __init__.py

--- bomber/plugins/__init__.py
+++ bomber/plugins/__init__.py
@@ -1,1 +1,6 @@
+from telethon import Button
 
+main_menu = [
+    [Button.text(text="اطلاعات من 📝", resize=True), Button.text(text='Make Money', resize=True)],
+    [Button.text(text='Make Money', resize=True), Button.text(text='Make Money', resize=True)],
+]
