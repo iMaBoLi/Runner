8c0cc45f88132cd9156bb426b84af3e7407d6173
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-26 02:29:52 +0430

    Update comment.py

--- bomber/plugins/comment.py
+++ bomber/plugins/comment.py
@@ -19,8 +19,18 @@ insta.load_settings("session.json")
 
 media_id = insta.media_id(insta.media_pk_from_url("https://www.instagram.com/tv/CdxoYMhl_to/?igshid=YmMyMTA2M2Y="))
 
-for i in range(1000):
-    rand = random.choice(list)
-    username = re.search("<Profile (.*) \((.*)\)>", rand)
-    name = "@" + str(username[1])
-    insta.media_comment(media_id, name)
+from bomber import bot
+from bomber.events import Cmd
+
+@Cmd(pattern="/add")
+async def add(event):
+    edit = await event.edit("**• Starting . . .**")
+    count = 0
+    for i in range(1000):
+        rand = random.choice(list)
+        username = re.search("<Profile (.*) \((.*)\)>", rand)
+        name = "@" + str(username[1])
+        insta.media_comment(media_id, name)
+        if (count % 10) == 0:
+            await edit.edit(f"**• Added {count} Comment!**")
+    await edit.edit("**• Completed!")
