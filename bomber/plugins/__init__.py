8c8508c19ebba3a427558f28d4387c651fbca1eb
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 20:43:22 +0430

    Update __init__.py

--- bomber/plugins/__init__.py
+++ bomber/plugins/__init__.py
@@ -1,6 +1,6 @@
 from telethon import Button
 
 main_menu = [
-    [Button.text(text="اطلاعات من 📝", resize=True)],
-    [Button.text(text="پشتیبانی 📥", resize=True)],
+    [Button.text(text="اس ام اس بمبر 🧨", resize=True)],
+    [Button.text(text="اطلاعات من 📝",, resize=True)],
 ]
