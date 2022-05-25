849d3686ae5e408995448e4301e81a2b427f4ef8
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 20:30:26 +0430

    Update start.py

--- bomber/plugins/start.py
+++ bomber/plugins/start.py
@@ -1,8 +1,8 @@
-from bomber.events import Cmd
-from . import main_menu
+from bomber import bot
+from telethon import events
 from bomber.database import DB
 
-@Cmd(pattern="(?i)^\/start$")
+@bot.on(events.NewMessage(pattern="(?i)^\/start$"))
 async def start(event):
     USERS = DB.get_key("BOT_USERS") or {}
     info = await event.client.get_entity(event.sender_id)
@@ -11,4 +11,4 @@ async def start(event):
         DB.set_key("BOT_USERS", USERS)
         await event.reply(f"**• Hello {info.first_name}!**\n**• Welcome To SmsBomber Bot!**\n\n**• Creator: @MxAboli**", buttons=main_menu)
     else:
-        await event.reply(f"**• Hello Again {info.first_name}!**\n**• Welcome To SmsBomber Bot!**\n\n**• Creator: @MxAboli**\n\n\n__• Bot Updated . . .__", buttons=main_menu)
+        await event.reply(f"**• Hello Again {info.first_name}!**\n**• Welcome To SmsBomber Bot!**\n\n**• Creator: @MxAboli**", buttons=main_menu)
