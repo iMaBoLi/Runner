658dc996d5df9a3155320d25b284a66dc0f27da2
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 19:11:15 +0430

    Update __main__.py

--- bomber/__main__.py
+++ bomber/__main__.py
@@ -1,11 +1,12 @@
 from . import bot
-from bot.functions.utils import load_plugins
+from bomber.functions.utils import load_plugins
 
 async def setup():
     LOGS.info("• Starting Setup Plugins . . .")
-    load_plugins("plugins")
+    load_plugins()
     LOGS.info("• Setup Plugins Completed!")
     bot.me = await bot.get_me()
-    LOGS.info("• Alien UserBot Has Been Start Now!")
+    LOGS.info("• Bomber Has Been Start Now!")
 
 bot.loop.run_until_complete(setup())
+bot.run_until_disconnected()
