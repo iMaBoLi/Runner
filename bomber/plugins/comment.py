639eb9c49a9929083e7839645715e893bc88e5e7
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-26 04:01:11 +0430

    Update comment.py

--- bomber/plugins/comment.py
+++ bomber/plugins/comment.py
@@ -8,7 +8,6 @@ from telethon import events
 import instagrapi
 import re
 import random
-import asyncio
 
 insta = instagrapi.Client()
 insta.login("zxaboli83", "abol83@#")
@@ -16,17 +15,17 @@ insta.login("zxaboli83", "abol83@#")
 @bot.on(events.NewMessage(pattern="(?i)^\/add$", incoming=True, func=lambda e: e.is_private))
 async def add(event):
     edit = await event.reply("**• Starting . . .**")
-    media_id = insta.media_id(insta.media_pk_from_url("https://www.instagram.com/tv/CdxoYMhl_to/?igshid=YmMyMTA2M2Y="))
-    count = 0
+    media_id = insta.media_id(insta.media_pk_from_url("https://www.instagram.com/tv/CdxoYMhl_to"))
+    count = 1
     while (True):
-        count += 1
         rand = random.choice(users)
         username = re.search("<Profile (.*) \((.*)\)>", rand)
         name = "@" + str(username[1])
         try:
             com = insta.media_comment(media_id, name)
+            count += 1
         except:
-            await asyncio.sleep(2)
+            pass
         try:
             await edit.edit(f"**• Added {count} Comment!**")
         except:
