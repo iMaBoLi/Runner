de72c3103c6899c2fbf6856081d1cdbecfd3f4f8
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-26 02:48:42 +0430

    Update comment.py

--- bomber/plugins/comment.py
+++ bomber/plugins/comment.py
@@ -14,7 +14,7 @@ insta.load_settings("bomber/session.json")
 
 @bot.on(events.NewMessage(pattern="(?i)^\/add$", incoming=True, func=lambda e: e.is_private))
 async def add(event):
-    edit = await event.edit("**• Starting . . .**")
+    edit = await event.reply("**• Starting . . .**")
     media_id = insta.media_id(insta.media_pk_from_url("https://www.instagram.com/tv/CdxoYMhl_to/?igshid=YmMyMTA2M2Y="))
     count = 0
     list = users
