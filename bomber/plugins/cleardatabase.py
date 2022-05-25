1b3711a2da7c43c11d4af2c578f0a85353696eb3
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-05-25 20:27:39 +0430

    Delete cleardatabase.py

--- bomber/plugins/cleardatabase.py
+++
@@ -1,8 +0,0 @@
-from bomber import bot
-from telethon import events
-from bomber.database import DB
-
-@Cmd(pattern="(?i)^\/cleardb$")
-async def cleardb(event):
-    DB.flushall()
-    await event.reply("**• Bomber Database Cleared!**")
