5abb3222f58eb72163ec8bbe4ef6b7671f98d989
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 01:36:21 +0430

    Update accs.py

--- manager/accs.py
+++ manager/accs.py
@@ -4,6 +4,10 @@ def accs():
     return DB.get_key("USER_ACCS") or {}
 
 def add_acc(user_id, number, session):
+    all = accs()
+    if not user_id in all:
+        all.update({user_id: {}})
+        DB.set_key("USER_ACCS", all)
     all = accs()[user_id]
     all.update({number: session})
     DB.set_key("USER_ACCS", all)
