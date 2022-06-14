34aa58a3b67bedc0f1bdfb59e2f8f2a5e8eb9370
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-14 21:55:56 +0430

    Update __main__.py

--- bomber/__main__.py
+++ bomber/__main__.py
@@ -1,13 +1,25 @@
 from . import bot, ADMIN_ID
-from bomber.functions.utils import load_plugins
 
 async def setup():
     print("• Starting Setup Plugins . . .")
-    load_plugins()
+    files = glob.glob(f"manager/plugins/*.py")
+    for name in files:
+        plugin_name = os.path.basename(name)
+        try:
+            path = Path(f"manager/plugins/{plugin_name}")
+            name = "manager.plugins.{}".format(plugin_name.replace(".py" , ""))
+            spec = importlib.util.spec_from_file_location(name, path)
+            load = importlib.util.module_from_spec(spec)
+            load.logger = logging.getLogger(plugin_name)
+            spec.loader.exec_module(load)
+            sys.modules[name] = load
+            print(f"""• Bot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
+        except Exception as e:
+            print(f"""• Bot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
     print("• Setup Plugins Completed!")
     bot.me = await bot.get_me()
     bot.admin = await bot.get_entity(ADMIN_ID)
-    print("• Bomber Has Been Start Now!")
+    print("• Bot Has Been Start Now!")
 
 bot.loop.run_until_complete(setup())
 bot.run_until_disconnected()
