920ab05bd6e9408a78fbbb4ecc70c47a3b8e356d
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 00:16:01 +0430

    Create functions.py

---
+++ manager/functions.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+def search_photo(query):
+    query = query.replace(" ", "-")
+    link = "https://unsplash.com/s/photos/" + query
+    extra = requests.get(link).content
+    res = BeautifulSoup(extra, "html.parser", from_encoding="utf-8")
+    all = res.find_all("img", "YVj9w")
+    return [image["src"] for image in all]
