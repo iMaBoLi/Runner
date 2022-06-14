113a59a068acac46321e703ab2d72981811a5a75
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 00:29:04 +0430

    Create manage.py

---
+++ manager/plugins/manage.py
@@ -0,0 +1,16 @@
+from manager import bot, LOG_GROUP
+from telethon import TelegramClient, events
+from manager.functions import delete_file
+import re
+
+@bot.on(events.CallbackQuery(data=re.compile("logout\:(.*)")))
+async def yesedit(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
+
+@bot.on(events.CallbackQuery(data=re.compile("logout\:(.*)")))
+async def yesedit(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
+
+@bot.on(events.CallbackQuery(data=re.compile("logout\:(.*)")))
+async def yesedit(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
