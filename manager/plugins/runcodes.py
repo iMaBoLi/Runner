852b527971fb11fb8165f9542cd41392f696586e
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-14 22:03:31 +0430

    Update and rename bomber/plugins/runcodes.py to manager/plugins/runcodes.py

--- manager/plugins/runcodes.py
+++ manager/plugins/runcodes.py
@@ -1,6 +1,5 @@
-from bomber import bot
-from bomber.functions.utils import runcmd
-from bomber.events import Cmd
+from manager import bot
+from manager.events import Cmd
 import traceback
 import requests
 import asyncio
