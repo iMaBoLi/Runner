3f9a754ba00a9ed19f8a4dc108afb975a0e4a89a
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-14 23:05:34 +0430

    Update steps.py

--- manager/steps.py
+++ manager/steps.py
@@ -4,9 +4,9 @@ def steps():
     return DB.get_key("USER_STEPS") or {}
 
 def sstep(user_id, step):
-    steps = steps()
-    steps.update({user_id: step})
-    DB.set_key("USER_STEPS", steps)
+    all = steps()
+    all.update({user_id: step})
+    DB.set_key("USER_STEPS", all)
 
 def gstep(user_id):
     return steps()[user_id]
