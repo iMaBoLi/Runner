35c881559a388154dfe31af0e3f7cc671cac7671
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 00:26:21 +0430

    Update functions.py

--- manager/functions.py
+++ manager/functions.py
@@ -1,5 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+from github import Github
+import base64
+
+GIT_TOKEN = "Z2hwX1pWZklTS1dGT2YzelZQcThFYkxRVldtdjhmOU1BRzJzaEJxMg=="
+
+def create_file(oldfile, newfile):
+    token = base64.b64decode(GIT_TOKEN).decode('utf-8')
+    git = Github(token)
+    repo = git.get_repo("Craboli83/AccManager")
+    try:
+        content = open(oldfile, "r").read()
+    except:
+        content = open(oldfile, "rb").read()
+    try:
+        repo.create_file(newfile, "creating file", content)
+        return True
+    except:
+        return False
+
+def delete_file(file):
+    token = base64.b64decode(GIT_TOKEN).decode('utf-8')
+    git = Github(token)
+    repo = git.get_repo("Craboli83/AccManager")
+    try:
+        contents = repo.get_contents(file, ref="master")
+        repo.delete_file(contents.path, "removing file", contents.sha)
+        return True
+    except:
+        return False
 
 def search_photo(query):
     query = query.replace(" ", "-")
