d440d24f90faa4c62efea06aed4cb0c7991881b7
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 12:28:36 +0430

    Update acc_settings.py

--- manager/plugins/acc_settings.py
+++ manager/plugins/acc_settings.py
@@ -42,4 +42,4 @@ async def change_set(event):
         [Button.inline(f"{ch_uname} Change Username {ch_uname}", data=f"ch_uname:{event.sender_id}")],
         [Button.inline(f"{ch_photo} Change Photo {ch_photo}", data=f"ch_photo:{event.sender_id}")],
     ]
-    await event.edit(event, buttons=buttons)
+    await event.edit(event.text, buttons=buttons)
