5550ffb63c00f045d2adf176b90a1e5d85f07b5b
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 12:58:03 +0430

    Update start.py

--- manager/plugins/start.py
+++ manager/plugins/start.py
@@ -1,11 +1,13 @@
 from manager import bot
 from manager.events import Cmd
 from . import main_menu
+from manager.database.steps import sstep
 
 @Cmd(pattern="(?i)^\/start$")
 async def start(event):
+    sstep(event.sender_id, "free")
     info = await event.client.get_entity(event.sender_id)
-    await event.reply(f"**👋 Hi {info.mention}!**\n**😘 Welcome To My Acc Manager Robot!**\n\n**💡 Maker: @{bot.admin.username}**", buttons=main_menu)
+    await event.reply(f"**👋 Hi {info.mention}!**\n**😘 Welcome To Acc Manager Robot!**\n\n**💡 Maker: @{bot.admin.username}**", buttons=main_menu)
 
 @Cmd(pattern="⬅️ Back")
 async def start(event):
