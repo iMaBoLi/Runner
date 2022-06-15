3a2668a54d03689f76c29ad957f5faef40d17d4a
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 19:35:19 +0430

    Update manage.py

--- manager/plugins/manage.py
+++ manager/plugins/manage.py
@@ -11,6 +11,14 @@ async def logout(event):
     client = TelegramClient(StringSession(session), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
     await client.connect()
     await client.log_out()
+    allaccs = DB.get_key("USER_ACCS")[event.sender_id]
+    if phone in allaccs:
+        all = DB.get_key("USER_ACCS_COUNT")
+        all[event.sender_id] -= 1
+        DB.set_key("USER_ACCS_COUNT", all)
+    allaccs = DB.get_key("USER_ACCS")
+    del allaccs[event.sender_id][phone]
+    DB.set_key("USER_ACCS", allaccs)
     await event.edit(f"**• Im LogOut From Your Account!**\n\n**• Account Number:** ( `{phone}` )")
 
 @bot.on(events.CallbackQuery(data=re.compile("getcodes\:(.*)")))
@@ -25,8 +33,8 @@ async def getcodes(event):
         if match:= re.search("(\d*)\.", mes.text):
             if match.group(1):
                 codes += f"**• {count} -**  `{match.group(1)}`\n"
+                count += 1
     await bot.send_message(event.chat_id, codes)
-    await event.answer("• Telegram Codes Sneded For You!", alert=True)
 
 @bot.on(events.CallbackQuery(data=re.compile("resetauthorization\:(.*)")))
 async def resetauthorization(event):
