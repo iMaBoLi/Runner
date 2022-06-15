575a7378c563772b7431863f4a76467562bf9fff
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 10:03:59 +0430

    Create __init__.py

---
+++ manager/plugins/__init__.py
@@ -0,0 +1,6 @@
+from telethon import Button
+
+main_menu = [
+    [Button.text("Add Account 📥", resize=True, single_use=True)],
+    [Button.text("My Info 📝", resize=True, single_use=True), Button.text("Support 🧑‍💻", resize=True, single_use=True)],
+]
