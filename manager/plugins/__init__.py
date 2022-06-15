ffa714df7f4f795660b282a9eeea09843bf71c3d
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 12:38:06 +0430

    Update __init__.py

--- manager/plugins/__init__.py
+++ manager/plugins/__init__.py
@@ -2,6 +2,7 @@ from telethon import Button
 
 main_menu = [
     [Button.text("Add Account 📥", resize=True, single_use=True)],
+    [Button.text("Accs Setting ⚙️", resize=True, single_use=True)],
     [Button.text("My Info 📝", resize=True, single_use=True), Button.text("Support 🧑‍💻", resize=True, single_use=True)],
 ]
 
