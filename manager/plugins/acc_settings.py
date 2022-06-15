a993442918f346502951abf3213698de7bfb3de3
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 12:19:56 +0430

    Update acc_settings.py

--- manager/plugins/acc_settings.py
+++ manager/plugins/acc_settings.py
@@ -20,3 +20,27 @@ async def acc_settings(event):
         [Button.inline(f"{ch_photo} Change Photo {ch_photo}", data=f"ch_photo:{event.sender_id}")],
     ]
     await event.reply(f"**• Welcome To Setting Panel For Your Accounts:**\n\n__• Edit Your Settings:__", buttons=buttons)
+
+@bot.on(events.CallbackQuery(data=re.compile("ch\_(.*)\:(.*)")))
+async def change_set(event):
+    type = str(event.pattern_match.group(1).decode('utf-8'))
+    id = str(event.pattern_match.group(2).decode('utf-8'))
+    if type == "fname":
+        all = DB.get_key("CHANGE_ACCS_FNAME")
+        last = "no" if DB.get_key("CHANGE_ACCS_FNAME")[event.sender_id] == "yes" else "yes"
+        all[event.sender_id] = last
+        DB.set_key("CHANGE_ACCS_FNAME", all)
+    ch_fname = "✅" if DB.get_key("CHANGE_ACCS_FNAME")[event.sender_id] == "yes" else "❌"
+    ch_lname = "✅" if DB.get_key("CHANGE_ACCS_LNAME")[event.sender_id] == "yes" else "❌"
+    ch_bio = "✅" if DB.get_key("CHANGE_ACCS_BIO")[event.sender_id] == "yes" else "❌"
+    ch_uname = "✅" if DB.get_key("CHANGE_ACCS_USERNAME")[event.sender_id] == "yes" else "❌"
+    ch_photo = "✅" if DB.get_key("CHANGE_ACCS_PHOTO")[event.sender_id] == "yes" else "❌"
+    buttons = [
+        [Button.inline(f"{ch_fname} Change FirstName {ch_fname}", data=f"ch_fname:{event.sender_id}")],
+        [Button.inline(f"{ch_lname} Change LastName {ch_lname}", data=f"ch_lname:{event.sender_id}")],
+        [Button.inline(f"{ch_bio} Change Bio {ch_bio}", data=f"ch_bio:{event.sender_id}")],
+        [Button.inline(f"{ch_uname} Change Username {ch_uname}", data=f"ch_uname:{event.sender_id}")],
+        [Button.inline(f"{ch_photo} Change Photo {ch_photo}", data=f"ch_photo:{event.sender_id}")],
+    ]
+    event.buttons = buttons
+    await event.edit(event)
