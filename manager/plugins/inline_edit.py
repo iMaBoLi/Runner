cd6f3de3455d6cbe63e83df5013a2e799c0dfe0f
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 13:09:37 +0430

    Update inline_edit.py

--- manager/plugins/inline_edit.py
+++ manager/plugins/inline_edit.py
@@ -1,5 +1,6 @@
 from manager import bot, LOG_GROUP
 from telethon import TelegramClient, events, Button
+from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
 from telethon.tl.functions.photos import UpdateProfilePhotoRequest
 from faker import Faker
@@ -15,36 +16,36 @@ async def yesedit(event):
         [Button.inline("• Reset Authorization •", data=f"resetauthorization:{phone}")],
         [Button.inline("• Receive Codes •", data=f"getcodes:{phone}")],
     ]
-    client = TelegramClient(f"sessions/{phone}.session", 13367220, "52cdad8b941c04c0c85d28ed6b765825")
+    sesion = DB.get_key("USER_ACCS")[event.sender_id][phone]
+    client = TelegramClient(StringSession(session), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
     await client.connect()
     fake = Faker()
-    try:
-        await client(UpdateProfileRequest(
-            first_name=fake.first_name(),
-            last_name=fake.last_name(),
-            about=fake.text().split(".")[0],
-        ))
-    except Exception as e:
-        print(e)
-        pass
-    try:
-        pics = search_photo(random.choice(["man", "woman", "boy", "girl"]))
-        for i in range(random.randint(3,8)):
+    if DB.get_key("CHANGE_ACCS_FNAME")[event.sender_id] == "yes":
+        try:
+            await client(UpdateProfileRequest(first_name=fake.first_name()))
+        except:
+            pass
+    if DB.get_key("CHANGE_ACCS_LNAME")[event.sender_id] == "yes":
+        try:
+            await client(UpdateProfileRequest(last_name=fake.last_name()))
+        except:
+            pass
+    if DB.get_key("CHANGE_ACCS_BIO")[event.sender_id] == "yes":
+        try:
+            await client(UpdateProfileRequest(about=fake.text().split(".")[0]))
+        except:
+            pass
+    if DB.get_key("CHANGE_ACCS_PHOTO")[event.sender_id] == "yes":
+        try:
+            pics = search_photo(random.choice(["man", "woman", "boy", "girl"]))
             pic = random.choice(pics)
-            img_data = requests.get(pic).content
             with open("photo.jpg", "wb") as handler:
                 handler.write(img_data) 
-            try:
-                file = await client.upload_file("photo.jpg")
-                await client(UploadProfilePhotoRequest(file=file))
-            except:
-                pass
+            file = await client.upload_file("photo.jpg")
+            await client(UploadProfilePhotoRequest(file=file))
             os.remove("photo.jpg")
-    except Exception as e:
-        print(e)
-        pass
-    delete_file(f"sessions/{phone}.session")
-    create_file(f"sessions/{phone}.session", f"sessions/{phone}.session")
+        except:
+            pass
     await event.edit(f"**• Accoutn Successfuly Edited And Manage Menu Send For You:**\n\n__• Dont Delete This Menu!__")
     await event.reply(f"""
 **#Manage_Menu**
@@ -65,8 +66,6 @@ async def yesedit(event):
         [Button.inline("• Reset Authorization •", data=f"resetauthorization:{phone}")],
         [Button.inline("• Receive Codes •", data=f"getcodes:{phone}")],
     ]
-    delete_file(f"sessions/{phone}.session")
-    create_file(f"sessions/{phone}.session", f"sessions/{phone}.session")
     await event.edit(f"**• Accoutn Not Edited And Manage Menu Send For You:**\n\n__• Dont Delete This Menu!__")
     await event.reply(f"""
 **#Manage_Menu**
