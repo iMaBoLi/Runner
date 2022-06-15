3f5fcb5790f6536f9b493b182597e3b9c1263be3
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-15 17:18:19 +0430

    Update add_account.py

--- manager/plugins/add_account.py
+++ manager/plugins/add_account.py
@@ -22,60 +22,53 @@ import requests
 import glob
 
 @Cmd(pattern="Add Account 📥")
-async def add(event):
-    await event.reply("**•Ok, Send Your Phone Number:**\n\n__• Ex: +19307777777 __", buttons=back_menu)
-    sstep(event.sender_id, "add_account")
-
-@Cmd()
 async def add_account(event):
-    if gstep(event.sender_id) == "add_account":
+    async with client.conversation(event.chat_id) as conv:
+        send = await event.reply("**•Ok, Send Your Phone Number:**\n\n__• Ex: +19307777777 __", buttons=back_menu)
+        response = await conv.get_response(send.id)
+        phone = response.text 
+    edit = await event.reply("`• Please Wait . . .`")
+    client = TelegramClient(StringSession(), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
+    await client.connect()
+    try:
+        scode = await client.send_code_request(phone)
+        async with client.conversation(event.chat_id) as conv:
+            send = await edit.edit(f"**• Ok, Send Your Telegram Code For:** ( `{phone}` )")
+            response = await conv.get_response(send.id)
+            phone_code = response.text 
+    except PhoneNumberInvalidError:
+        return await edit.edit("**• Your Phone Number Is Invalid!**")
+    except PhoneNumberFloodError:
+        return await edit.edit("**• Your Phone Number Is Flooded!**")
+    except PhoneNumberBannedError:
+        return await edit.edit("**• Your Phone Number Is Banned!**")
+    except TimeoutError:
+        return await edit.edit("**• Your Conversation Has Been Canceled, Try Again!**", buttons=main_menu)
+    edit = await event.reply("`• Please Wait . . .`")
+    phone_code = phone_code.replace(" ", "")
+    try:
+        await client.sign_in(phone, phone_code, password=None)
+        buttons = [[Button.inline("• Yes •", data=f"yesedit:{phone}"), Button.inline("• No •", data=f"noedit:{phone}")]]
+        session = client.session.save()
+        allaccs = DB.get_key("USER_ACCS")[event.sender_id]
+        if phone not in allaccs:
+            all = DB.get_key("USER_ACCS_COUNT")
+            all[event.sender_id] += 1
+            DB.set_key("USER_ACCS_COUNT", all)
+        allaccs = DB.get_key("USER_ACCS")
+        allaccs[event.sender_id][phone] = session
+        DB.set_key("USER_ACCS", allaccs)
+        await edit.edit(f"**• Successfuly Login To Your Account!**\n\n**• Your Session String:** ( ||{session}|| )\n\n**• Do You Want To Edit Your Account???**", buttons=buttons)
+    except PhoneCodeInvalidError:
+        return await edit.edit("**• Your Code Is Invalid!**\n\n__• Check Code Again!__")
+    except PhoneCodeExpiredError:
+        return await edit.edit("**• Your Code Is Expired, Try Again!**", buttons=main_menu)
+    except SessionPasswordNeededError:
+        async with client.conversation(event.chat_id) as conv:
+            send = await edit.edit(f"**• Ok, Send Your Account Password For:** ( `{phone}` )")
+            response = await conv.get_response(send.id)
+            password = response.text  
         edit = await event.reply("`• Please Wait . . .`")
-        phone = event.text
-        client = TelegramClient(StringSession(), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
-        await client.connect()
-        try:
-            scode = await client.send_code_request(phone)
-            async with client.conversation(event.chat_id) as conv:
-                send = await edit.edit(f"**• Ok, Send Your Telegram Code For:** ( `{phone}` )")
-                response = await conv.get_response(send.id)
-                phone_code = response.text 
-        except PhoneNumberInvalidError:
-            return await edit.edit("**• Your Phone Number Is Invalid!**")
-        except PhoneNumberFloodError:
-            return await edit.edit("**• Your Phone Number Is Flooded!**")
-        except PhoneNumberBannedError:
-            return await edit.edit("**• Your Phone Number Is Banned!**")
-        except TimeoutError:
-            sstep(event.sender_id, "free")
-            return await edit.edit("**• Your Conversation Has Been Canceled, Try Again!**", buttons=main_menu)
-        edit = await event.reply("`• Please Wait . . .`")
-        phone_code = event.text.replace(" ", "")
-        try:
-            await client.sign_in(phone, phone_code, phone_code_hash=phone_code_hash, password=None)
-            buttons = [[Button.inline("• Yes •", data=f"yesedit:{phone}"), Button.inline("• No •", data=f"noedit:{phone}")]]
-            session = client.session.save()
-            allaccs = DB.get_key("USER_ACCS")[event.sender_id]
-            if phone not in allaccs:
-                all = DB.get_key("USER_ACCS_COUNT")
-                all[event.sender_id] += 1
-                DB.set_key("USER_ACCS_COUNT", all)
-            allaccs = DB.get_key("USER_ACCS")
-            allaccs[event.sender_id][phone] = session
-            DB.set_key("USER_ACCS", allaccs)
-            await edit.edit(f"**• Successfuly Login To Your Account!**\n\n**• Your Session String:** ( ||{session}|| )\n\n**• Do You Want To Edit Your Account???**", buttons=buttons)
-            sstep(event.sender_id, "free")
-        except PhoneCodeInvalidError:
-            return await edit.edit("**• Your Code Is Invalid!**\n\n__• Check Code Again!__")
-        except PhoneCodeExpiredError:
-            sstep(event.sender_id, "free")
-            return await edit.edit("**• Your Code Is Expired, Try Again!**", buttons=main_menu)
-        except SessionPasswordNeededError:
-            return await edit.edit(f"**• Ok, Send Your Account Password For:** ( `{phone}` )")
-        edit = await event.reply("`• Please Wait . . .`")
-        phone = gstep(event.sender_id).split(":")[1]
-        client = TelegramClient(StringSession(), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
-        await client.connect()
-        password = event.text
         try:
             await client.sign_in(password=password)
             buttons = [[Button.inline("• Yes •", data=f"yesedit:{phone}"), Button.inline("• No •", data=f"noedit:{phone}")]]
@@ -91,4 +84,4 @@ async def add_account(event):
             await edit.edit(f"**• Successfuly Login To Your Account!**\n\n**• Your Session String:** ( ||{session}|| )\n\n**• Do You Want To Edit Your Account???**", buttons=buttons)
             sstep(event.sender_id, "free")
         except PasswordHashInvalidError:
-            return await edit.edit("**• Your Account Password Is Invalid!**")
+            return await edit.edit("**• Your Account Password Is Invalid, Try Again!**")
