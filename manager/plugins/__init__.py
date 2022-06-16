bf77115cf19bdcb9c32b7675fc73832f651832d5
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-16 21:12:16 +0430

    Update __init__.py

--- manager/plugins/__init__.py
+++ manager/plugins/__init__.py
@@ -13,6 +13,7 @@ back_menu = [
 def manage_menu(phone):
     menu = [
         [Button.inline("• LogOut Bot •", data=f"logout:{phone}")],
+        [Button.inline("• Reset Authorizations •", data=f"resauths:{phone}")],
         [Button.inline("• Get Authorizations •", data=f"getauths:{phone}"), Button.inline("• Get Telegram Codes •", data=f"getcodes:{phone}")],
         [Button.inline("• Get Session File •", data=f"sesfile:{phone}"), Button.inline("• Get Telethon Session •", data=f"sestel:{phone}")],
     ]
