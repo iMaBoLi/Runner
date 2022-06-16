de5cff13490c686eaf40a56e31daa3af2c77c9ab
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-17 00:47:32 +0430

    Update __init__.py

--- manager/__init__.py
+++ manager/__init__.py
@@ -5,6 +5,7 @@ API_ID = 18917792
 API_HASH = "1f1addf94481ee8ae1594ddc6ea78dfa"
 BOT_TOKEN = "5567035354:AAEzENilm6ToojFZdZuErdtP8hryvRHTVZI"
 LOG_GROUP = "MyLogAccManagerGroup"
+CHANNEL = "AccManager_Ch"
 ADMIN_ID = 5250298585
 
 print("• Starting Bot . . .")
