0bd6fca560cc4a6e97ac81a1e6de911fc964a29e
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-16 20:53:08 +0430

    Update functions.py

--- manager/functions.py
+++ manager/functions.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from github import Github
 import base64
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
 
 GIT_TOKEN = "Z2hwX1pWZklTS1dGT2YzelZQcThFYkxRVldtdjhmOU1BRzJzaEJxMg=="
 
@@ -37,3 +40,14 @@ def search_photo(query):
     res = BeautifulSoup(extra, "html.parser", from_encoding="utf-8")
     all = res.find_all("img", "YVj9w")
     return [image["src"] for image in all]
+
+async def sql_session(session, phone):
+      if os.path.exists(f"{phone}.session"):
+          os.remove(f"{phone}.session")
+      string = StringSession(session)
+      client = TelegramClient(f"{phone}.session", 13367220, "52cdad8b941c04c0c85d28ed6b765825")
+      client.session.set_dc(string.dc_id, string.server_address, string.port)
+      client.session.auth_key = string.auth_key
+      client.session.auth_key.key = string.auth_key.key
+      await client.connect()
+      return f"{phone}.session"
