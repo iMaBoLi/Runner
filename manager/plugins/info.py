3bd8aec6cbc621220b643c7d26cc4c82d7b03a07
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-16 19:06:45 +0430

    Update info.py

--- manager/plugins/info.py
+++ manager/plugins/info.py
@@ -1,9 +1,7 @@
-from manager import bot, LOG_GROUP
-from telethon import events, Button
+from manager import bot
 from manager.events import Cmd
 from manager.database import DB
 from time import gmtime, strftime
-import re
 
 @Cmd(pattern="My Info 📝")
 async def info(event):
@@ -22,18 +20,4 @@ async def info(event):
 
 __{date}__
 """
-    await event.reply(text, buttons=[[Button.inline("• Account List •", data=f"accs:{event.sender_id}")]])
-
-@bot.on(events.CallbackQuery(data=re.compile("accs\:(.*)")))
-async def acc_list(event):
-    id = int(event.pattern_match.group(1).decode('utf-8'))
-    accs = DB.get_key("USER_ACCS")[event.sender_id]
-    if len(accs) == 0:
-        return await event.answer("• Not Account Added To Bot!", alert=True)
-    text = "• Your Accounts List:\n\n"
-    count = 1
-    for acc in accs:
-        text += f"{count} - {acc}\n"
-        count += 1
-    open(f"{event.sender_id}.txt", "w").write(str(text))
-    await event.reply("**• Your Account List!**", file=f"{event.sender_id}.txt")
+    await event.reply(text)
