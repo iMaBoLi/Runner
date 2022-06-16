f1fcadf1f7a09bbae55fd4b3905324640faeb181
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-16 21:11:19 +0430

    Update manage.py

--- manager/plugins/manage.py
+++ manager/plugins/manage.py
@@ -2,6 +2,7 @@ from manager import bot, LOG_GROUP
 from telethon import TelegramClient, events, functions
 from telethon.sessions import StringSession
 from manager.database import DB
+from manager.functions import sql_session
 import re
 
 @bot.on(events.CallbackQuery(data=re.compile("logout\:(.*)")))
@@ -36,8 +37,8 @@ async def getcodes(event):
                 count += 1
     await bot.send_message(event.chat_id, codes)
 
-@bot.on(events.CallbackQuery(data=re.compile("resetauthorization\:(.*)")))
-async def resetauthorization(event):
+@bot.on(events.CallbackQuery(data=re.compile("resauths\:(.*)")))
+async def getauths(event):
     phone = str(event.pattern_match.group(1).decode('utf-8'))
     session = DB.get_key("USER_ACCS")[event.sender_id][phone]
     client = TelegramClient(StringSession(session), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
@@ -50,3 +51,31 @@ async def resetauthorization(event):
             await client(functions.account.ResetAuthorizationRequest(hash=acc.hash))
             cur += 1
     await event.answer(f"• Ok, {cur} Session From {all} Sessions Has Been Removed!", alert=True)
+
+@bot.on(events.CallbackQuery(data=re.compile("getauths\:(.*)")))
+async def getauths(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
+    session = DB.get_key("USER_ACCS")[event.sender_id][phone]
+    client = TelegramClient(StringSession(session), 13367220, "52cdad8b941c04c0c85d28ed6b765825")
+    await client.connect()
+    accs = await client(functions.account.GetAuthorizationsRequest())
+    all = len(accs.authorizations)
+    cur = 0
+    for acc in accs.authorizations:
+        if not acc.current:
+            await client(functions.account.ResetAuthorizationRequest(hash=acc.hash))
+            cur += 1
+    await event.answer(f"• Ok, {cur} Session From {all} Sessions Has Been Removed!", alert=True)
+
+@bot.on(events.CallbackQuery(data=re.compile("sesfile\:(.*)")))
+async def getauths(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
+    session = DB.get_key("USER_ACCS")[event.sender_id][phone]
+    file = await sql_session(session, phone)
+    await event.reply(f"**• Session File For This Number:** ( `{phone}` )", file=file)
+
+@bot.on(events.CallbackQuery(data=re.compile("sestel\:(.*)")))
+async def getauths(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
+    session = DB.get_key("USER_ACCS")[event.sender_id][phone]
+    await event.reply(f"**• Your Phone Is:** ( `{phone}` )\n\n**• Telethon String Session:** ( `{session}` )")
