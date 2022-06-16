78cffd40fa92b5d541833a547a7da28c1cce7966
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-16 19:17:48 +0430

    Update start.py

--- manager/plugins/start.py
+++ manager/plugins/start.py
@@ -7,6 +7,6 @@ async def start(event):
     info = await bot.get_entity(event.sender_id)
     await event.reply(f"**👋 Hi {info.first_name}!**\n**😘 Welcome To Acc Manager Robot!**\n\n**💡 Maker: @{bot.admin.username}**", buttons=main_menu)
 
-@Cmd(pattern="⬅️ Back")
-async def start(event):
-    await event.reply("**• Successfuly Backed To Home Page!**", buttons=main_menu)
+@Cmd(pattern="🔙")
+async def back(event):
+    await event.reply("**• Ok, Backed To Home Page!**", buttons=main_menu)
