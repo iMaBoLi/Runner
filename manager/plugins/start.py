103c3ee42467482351131a27b8d12498a9ae7388
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-17 01:04:43 +0430

    Update start.py

--- manager/plugins/start.py
+++ manager/plugins/start.py
@@ -1,6 +1,9 @@
-from manager import bot
+from manager import bot, CHANNEL
+from telethon import events, functions
+from . import main_menu, manage_menu
 from manager.events import Cmd
 from . import main_menu
+import re
 
 @Cmd(pattern="(?i)^\/start$")
 async def start(event):
@@ -11,9 +14,16 @@ async def start(event):
 async def back(event):
     await event.reply("**• Ok, Backed To Home Page!**", buttons=main_menu)
 
-            try:
-                await bot(functions.channels.GetParticipantRequest(
-                    channel=CHANNEL,
-                    participant=event.sender_id
-                ))
-            except:
+@bot.on(events.CallbackQuery(data=re.compile("checkjoin\:(.*)")))
+async def checkjoin(event):
+    id = int(event.pattern_match.group(1).decode('utf-8'))
+    try:
+        await bot(functions.channels.GetParticipantRequest(
+            channel=CHANNEL,
+            participant=id
+        ))
+        info = await bot.get_entity(event.sender_id)
+        await event.reply(f"**👋 Hi {info.first_name}!**\n**😘 Welcome To Acc Manager Robot!**\n\n**💡 Maker: @{bot.admin.username}**", buttons=main_menu)
+        await event.delete()
+    except:
+        await event.answer("• You Are Not Joined To Channel!", alert=True)
