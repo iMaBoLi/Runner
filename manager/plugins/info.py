5ba7557d850f4279beda6a94bec78e8d75abd1b0
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-16 09:16:48 +0430

    Update info.py

--- manager/plugins/info.py
+++ manager/plugins/info.py
@@ -2,19 +2,25 @@ from manager import bot, LOG_GROUP
 from telethon import events, Button
 from manager.events import Cmd
 from manager.database import DB
+from time import gmtime, strftime
 import re
 
 @Cmd(pattern="My Info 📝")
 async def info(event):
     info = await bot.get_entity(event.sender_id)
-    acc_count = DB.get_key("USER_ACCS_COUNT")[event.sender_id]
+    acc_count = len(DB.get_key("USER_ACCS")[event.sender_id])
+    date = strftime("%Y/%m/%d - %H:%M:%S", gmtime())
     text = f"""
 **• Your Information:**
 
 **• Name:** ( `{info.first_name}` )
+
 **• UserID:** ( `{info.id}` )
 
+
 **• Accounts Count:** ( `{acc_count}` )
+
+__{date}__
 """
     await event.reply(text, buttons=[[Button.inline("• Account List •", data=f"accs:{event.sender_id}")]])
 
@@ -27,7 +33,7 @@ async def acc_list(event):
     text = "• Your Accounts List:\n\n"
     count = 1
     for acc in accs:
-        text += "{count} - {acc}"
+        text += f"{count} - {acc}\n"
         count += 1
     open(f"{event.sender_id}.txt", "w").write(str(text))
     await event.reply("**• Your Account List!**", file=f"{event.sender_id}.txt")
