66dfad4b58025e2fc17a3302272713956b0de1d7
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-17 17:07:51 +0430

    Update events.py

--- manager/events.py
+++ manager/events.py
@@ -77,10 +77,7 @@ def Cmd(
                 buttons = [[Button.url("• Join Channel •", f"https://t.me/{CHANNEL}")], [Button.inline("Check Join ✅", data=f"checkjoin:{event.sender_id}")]]
                 return await event.reply(text, buttons=buttons)
 
-            USER_OFF_STATUS = DB.get_key("USER_OFF_STATUS") or []
-            if DB.get_key("BOT_STATUS") == "off" and not event.sender_id == bot.admin.id and event.sender_id not in USER_OFF_STATUS:
-                USER_OFF_STATUS.append(event.sender_id)
-                DB.set_key("USER_OFF_STATUS", USER_OFF_STATUS)
+            if DB.get_key("BOT_STATUS") == "off" and not event.sender_id == bot.admin.id:
                 return await event.reply("**• Sorry, The Bot Has Been DeActived ❌!**\n\n__• Please Try Again Later!__")
 
             try:
