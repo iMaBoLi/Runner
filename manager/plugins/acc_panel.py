a05a2830ba2cbe41800d611b94071fe1ffdc845c
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-17 17:44:45 +0430

    Update acc_panel.py

--- manager/plugins/acc_panel.py
+++ manager/plugins/acc_panel.py
@@ -7,13 +7,13 @@ from manager.database import DB
 
 @Cmd(pattern="Account Panel 🛠️")
 async def acc_panel(event):
+    accs = DB.get_key("USER_ACCS")[event.sender_id]
     async with bot.conversation(event.chat_id) as conv:
         send = await event.reply("**•Ok, Send Your Phone Number To Get Panel For This:**\n\n__• Ex: +19307777777 __", buttons=back_menu)
         response = await conv.get_response(send.id)
         phone = response.text
     if phone == "🔙":
         return
-    accs = DB.get_key("USER_ACCS")[event.sender_id]
     if phone not in accs:
         return await event.reply(f"**• You Are Not Added This Phone Number:** ( `{phone}` ) **To Bot!**")
     edit = await event.reply("`• Please Wait . . .`")
