511d207d112f36be78d6223beaf3919e2039f625
Abolfazl Farzin <96248859+Craboli83@users.noreply.github.com>
2022-06-17 17:43:22 +0430

    Update acc_list.py

--- manager/plugins/acc_list.py
+++ manager/plugins/acc_list.py
@@ -9,15 +9,19 @@ async def myaccs(event):
     accs = DB.get_key("USER_ACCS")[event.sender_id]
     if len(accs) == 0:
         return await event.reply("**• You Are Not Account Added To Bot!**")
-    text = "• Your Accounts List:\n\n"
-    count = 1
-    for acc in accs:
-        text += f"{count} - {acc}\n"
-        count += 1
-    open(f"{event.sender_id}.txt", "w").write(str(text))
-    text = f"""
-**• Your Accounts List:**
-
-**• Count:** ( `{len(accs)}` )
-"""
-    await event.reply(text, file=f"{event.sender_id}.txt")
+    elif len(accs) > 100:
+        text = f"**• Your Accounts List:**\n**• Count:** ( `{len(accs)}` )\n\n"
+        count = 1
+        for acc in accs:
+            text += f"**{count} -** `{acc}`\n"
+            count += 1
+        await event.reply(text)
+    else:
+        text = "• Your Accounts List:\n\n"
+        count = 1
+        for acc in accs:
+            text += f"{count} - {acc}\n"
+            count += 1
+        open(f"{event.sender_id}.txt", "w").write(str(text))
+        text = f"**• Your Accounts List:**\n\n**• Count:** ( `{len(accs)}` )"
+        await event.reply(text, file=f"{event.sender_id}.txt")
