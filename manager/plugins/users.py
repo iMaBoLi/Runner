74735aa647299c92e922513ceede2ee5deffe601
• imAboli <96248859+imAboli@users.noreply.github.com>
2022-06-23 01:54:30 +0430

    Update users.py

--- manager/plugins/users.py
+++ manager/plugins/users.py
@@ -13,7 +13,7 @@ async def block(event):
         await event.edit(f"**🚫 This User:** ( `{userid}` ) **Successfuly Blocked From Bot!**")
         await bot.send_message(userid, "**🚫 You Have Blocked From Bot, Don't Use From Bot!**")
     else:
-        await event.edit(f"**🚫 This User:** ( `{userid}` ) **Already Blocked From Bot!**")
+        await event.edit(f"**🚫 This User** ( `{userid}` ) **Already Blocked From Bot!**")
 
 @bot.on(events.CallbackQuery(data=re.compile("unblock\:(.*)")))
 async def block(event):
@@ -25,4 +25,4 @@ async def block(event):
         await event.edit(f"**✅ This User:** ( `{userid}` ) **Successfuly UnBlocked From Bot!**")
         await bot.send_message(userid, "**✅ You Have UnBlocked From Bot, Use This Bot!**")
     else:
-        await event.edit(f"**✅ This User:** ( `{userid}` ) **Already UnBlocked From Bot!**")
+        await event.edit(f"**✅ This User** ( `{userid}` ) **Already UnBlocked From Bot!**")
