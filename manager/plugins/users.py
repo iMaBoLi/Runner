d526efabeecbfe14a83d53a1b4c4226788c9b239
• imAboli <96248859+imAboli@users.noreply.github.com>
2022-06-23 01:54:07 +0430

    Update users.py

--- manager/plugins/users.py
+++ manager/plugins/users.py
@@ -10,19 +10,19 @@ async def block(event):
     if not userid in blocks:
         blocks.append(userid)
         DB.set_key("BLOCK_USERS", blocks)
-        await event.edit(f"**🚫 This User:** `{userid}` **Successfuly Blocked From Bot!**")
+        await event.edit(f"**🚫 This User:** ( `{userid}` ) **Successfuly Blocked From Bot!**")
         await bot.send_message(userid, "**🚫 You Have Blocked From Bot, Don't Use From Bot!**")
     else:
-        await event.edit(f"**🚫 This User:** `{userid}` **Already Blocked From Bot!**")
+        await event.edit(f"**🚫 This User:** ( `{userid}` ) **Already Blocked From Bot!**")
 
 @bot.on(events.CallbackQuery(data=re.compile("unblock\:(.*)")))
 async def block(event):
     userid = int(event.pattern_match.group(1).decode('utf-8'))
     blocks = DB.get_key("BLOCK_USERS")
     if userid in blocks:
-        del blocks[userid]
+        blocks.remove(userid)
         DB.set_key("BLOCK_USERS", blocks)
-        await event.edit(f"**✅ This User:** `{userid}` **Successfuly UnBlocked From Bot!**")
+        await event.edit(f"**✅ This User:** ( `{userid}` ) **Successfuly UnBlocked From Bot!**")
         await bot.send_message(userid, "**✅ You Have UnBlocked From Bot, Use This Bot!**")
     else:
-        await event.edit(f"**✅ This User:** `{userid}` **Already UnBlocked From Bot!**")
+        await event.edit(f"**✅ This User:** ( `{userid}` ) **Already UnBlocked From Bot!**")
